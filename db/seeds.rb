94e5d2863fdd456d89034a3aec30b62bb5d3a7ec
Saki Jane <sakicanbereached@gmail.com>
2015-10-20 16:48:24 -0700

    Assignment 30 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,31 @@ posts = Post.all
 100.times do
   Comment.create!(
     post: posts.sample,
-    body: RandomDate.random_paragraph
+    body: RandomData.random_paragraph
   )
 end
+
+#1.times do
+  Post.find_or_create_by(title: "This is a unique title", body: "This is unique content") do |post|
+    post.Comment = "This is a unique comment"
+  end
+#end
+#  Post.find_or_create_by!(
+#    title: "This is a unique title",
+#    body: "This is unique content"
+#  )
+#end
+#post = Post.find_or_create_by!(
+#  title: "This is a unique title",
+#  body: "This is unique content"
+#)
+
+#1.times do
+#  Comment.find_or_create_by!(
+#    post: post,
+#    body: "This is a unique comment"
+#  )
+#end
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
