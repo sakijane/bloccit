58731fa9e19926266ad22aab3494b7e1e8e971d7
Saki Jane <sakicanbereached@gmail.com>
2015-10-21 14:04:43 -0700

    Assignment 31 completeted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  def spam_title
+    id % 5 == 0 ? "SPAM" : title
+  end
 end
+
+# post.each_with_index do { |x| x / 5 == Integer }
+#   post.title: "SPAM"
+#
