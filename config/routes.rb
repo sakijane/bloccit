9dc36333627efd7543d7a2fed81be4733a0345d7
Saki Jane <sakicanbereached@gmail.com>
2016-02-08 15:19:32 -0800

    Checkpoint 31 intro to crud in progress

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   get 'about' => 'welcome#about'
 
-  root to: 'welcome#index'
+  root 'welcome#index'
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -17,25 +17,25 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-#  describe "GET #show" do
-#    it "returns http success" do
-#      get :show
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+  #  describe "GET show" do
+   #    it "returns http success" do
+   #      get :show
+   #      expect(response).to have_http_status(:success)
+   #    end
+   #  end
 
-#  describe "GET #new" do
-#    it "returns http success" do
-#      get :new
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+   # describe "GET new" do
+   #   it "returns http success" do
+   #     get :new
+   #     expect(response).to have_http_status(:success)
+   #   end
+   # end
 
-#  describe "GET #edit" do
-#    it "returns http success" do
-#      get :edit
-#      expect(response).to have_http_status(:success)
-#    end
-#  end
+   #  describe "GET edit" do
+   #    it "returns http success" do
+   #      get :edit
+   #      expect(response).to have_http_status(:success)
+   #    end
+   #  end
 
 end
